363f8d8772b03d7ccac37a2648bc06f6a6c2787a
Md Mainul Islam Nerob <mdmainulislamnerob52@gmail.com>
2025-09-30 20:44:44 +0600

    fixed timer

--- household_servide/settings.py
+++ household_servide/settings.py
@@ -167,7 +167,7 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
-   "ACCESS_TOKEN_LIFETIME": timedelta(hours=20),
+   "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
 }
 
 DJOSER = {
